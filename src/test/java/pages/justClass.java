f15f3cd9e56550e29f3f42093b21b4833a1e426b
AikaKoch <11aizhamal11@gmail.com>
2023-10-04 07:25:10 -0500

    update

--- src/test/java/pages/justClass.java
+++ src/test/java/pages/justClass.java
@@ -1,2 +1,11 @@
-package pages;public class justClass {
+package pages;
+
+import org.junit.Test;
+
+public class justClass {
+
+    @Test
+    public void Class() {
+        System.out.println("just print");;
+    }
 }
